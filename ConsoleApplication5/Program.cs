804dc469b4bd2f1578f6350f27a056b35e5e7173
Christian Henriksen <crelde@hotmail.com>
2012-09-11 13:34:24 +0200

    EH ?

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+
+        /// <summary>
+        ///  OMG HI
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
